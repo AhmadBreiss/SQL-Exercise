04f53c23f4930d639a8eb2ad603a4af545fb8e1a
AhmadBreiss <ahmatobreiss@gmail.com>
2022-12-11 21:59:25 +0200

    completed_creating_table

--- answers.sh
+++ answers.sh
@@ -1,22 +1,15 @@
-# first step
-SELECT name from students
-# step2
-SELECT *  from students 
-WHERE age > 30
-# step3
-SELECT name FROM students 
-WHERE Gender='F' AND Age=30
-# step4
-SELECT Points FROM students 
-WHERE id=1
-# step5
-INSERT INTO students ( name, Age, Gender, Points)
-VALUES ('Ahmad', '21', 'M', '600')
-# step6
-UPDATE students
-SET Points = Points + 50
-WHERE ID = 2
-# step 7
-UPDATE students
-SET Points = Points - 50
-WHERE ID = 1
+# creating table
+CREATE TABLE graduates (
+ID INTEGER PRIMARY KEY AUTOINCREMENT,
+name TEXT UNIQUE,
+Age INTEGER,
+Gender TEXT,
+Points INTEGER,
+Graduation TEXT )
+# step10 & step11
+INSERT INTO graduates ( 'name', 'Age', 'Gender', 'Points', 'Graduation')
+VALUES ( 'Layal', '18', 'F', '350', '08/09/2018')
+# step12
+DELETE FROM students 
+WHERE ID=4
+
