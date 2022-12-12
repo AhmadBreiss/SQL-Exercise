c1d1d4e58dd97209ee41edb0ba9b94b5fd2c46bc
AhmadBreiss <ahmatobreiss@gmail.com>
2022-12-12 14:31:27 +0200

    completed_joins

--- answers.sh
+++ answers.sh
@@ -1,15 +1,15 @@
-# creating table
-CREATE TABLE graduates (
-ID INTEGER PRIMARY KEY AUTOINCREMENT,
-name TEXT UNIQUE,
-Age INTEGER,
-Gender TEXT,
-Points INTEGER,
-Graduation TEXT )
-# step10 & step11
-INSERT INTO graduates ( 'name', 'Age', 'Gender', 'Points', 'Graduation')
-VALUES ( 'Layal', '18', 'F', '350', '08/09/2018')
-# step12
-DELETE FROM students 
-WHERE ID=4
-
+# join
+# step 14
+SELECT employees.Name, employees.Company, companies.Date 
+FROM employees
+INNER JOIN companies ON companies.Name = employees.Company;
+# step15
+SELECT employees.Name
+FROM employees
+INNER JOIN companies ON companies.Name=employees.Company
+WHERE companies.Date<2000;
+# step16
+SELECT employees.Name
+FROM employees
+INNER JOIN companies ON companies.Name = employees.Company
+WHERE employees.Role='Graphic Designer';
