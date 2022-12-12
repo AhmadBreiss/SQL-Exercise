f0244d142b26f8bb7cd6657d92c069402b1b6afe
AhmadBreiss <ahmatobreiss@gmail.com>
2022-12-12 15:16:13 +0200

    completed_count&filter

--- answers.sh
+++ answers.sh
@@ -64,3 +64,23 @@ SELECT employees.Name
 FROM employees
 INNER JOIN companies ON companies.Name = employees.Company
 WHERE employees.Role='Graphic Designer';
+# count&filter
+# step18
+	SELECT name 
+FROM students 
+WHERE Points=(SELECT max (Points) FROM students);
+
+# step19
+SELECT avg(points) FROM students;
+
+# step20
+SELECT count (name) FROM students
+WHERE Points=500;
+
+step21
+SELECT name FROM students 
+WHERE name like '%s%';
+
+# step22
+SELECT * from students 
+ORDER by Points DESC 
