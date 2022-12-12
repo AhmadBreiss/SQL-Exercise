a2b3670e8a6b0bcfa0fd750c176e44553a6e8e5a
AhmadBreiss <ahmatobreiss@gmail.com>
2022-12-12 15:13:30 +0200

    completed_joins

--- answers.sh
+++ answers.sh
@@ -47,3 +47,20 @@ WHERE ID='1';
 # step12
 DELETE FROM students 
 WHERE id='4';
+# joins
+# step14
+SELECT employees.Name, employees.Company, companies.Date 
+FROM employees
+INNER JOIN companies ON companies.Name = employees.Company;
+
+# step15
+SELECT employees.Name
+FROM employees
+INNER JOIN companies ON companies.Name=employees.Company
+WHERE companies.Date<2000;
+
+# step16
+SELECT employees.Name
+FROM employees
+INNER JOIN companies ON companies.Name = employees.Company
+WHERE employees.Role='Graphic Designer';
